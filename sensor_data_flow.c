de28e4e6310af4ab5d2daa08ecb46bd4ddd96c2b
bostar <starbo.net@gmail.com>
2015-12-11 04:28:03 -0800

    fix with file_sensor_data

--- sensor_data_flow.c
+++ sensor_data_flow.c
@@ -6,9 +6,8 @@
 #include "pthread.h"
 #include "server_duty.h"
 #include "parking_state_management.h"
+#include "file_sensor_data.h"
 
-
-const int SENSOR_THRESHOLD[LOCK_NUM] =  {100,100,100,100};
 
 static sensor_data_t sensor_calibration[LOCK_NUM] = {
 	{0,0,0,0,PTHREAD_MUTEX_INITIALIZER},
@@ -28,13 +27,27 @@ void sensor_data_flow_thread(void)
 {
 	int sensor_xx, sensor_yy, sensor_zz;
  	int i;	
+	int16 threshold;
+	sensor_data_save_t *sensor_data_save;
 	pthread_mutex_init(&sensor_mutex, NULL);
 	pthread_cond_init(&sensor_cond, NULL);
+	for ( i = 0; i < LOCK_NUM ; i++)
+	{
+		sensor_data_save = readSensorData(i + 1);
+		if( sensor_data_save )
+		{
+			sensor_calibration[i].x = sensor_data_save->x;
+			sensor_calibration[i].y = sensor_data_save->y;
+			sensor_calibration[i].z = sensor_data_save->z;
+			sensor_calibration[i].threshold = sensor_data_save->threshold;
+		}
+		else
+			printf(RED"read node 0x%04x sensor saved data failed\r\n"NONE, i + 1);
+	}
 
 	while(1)
 	{
 		pthread_cond_wait(&sensor_cond, &sensor_mutex);
-//		for( i = 0 ; i < LOCK_NUM ; i++ )
 		i = requestAddress -1 ;
 		{
 			pthread_mutex_lock(&sensor_update[i].mux);
@@ -46,7 +59,8 @@ void sensor_data_flow_thread(void)
 			pthread_mutex_unlock(&sensor_calibration[i].mux);
 
 			printf(CYAN"NODE 0x%04x: sensor_xx: %d, sensor_yy: %d, sensor_zz: %d\r\n"NONE,i+1,sensor_xx,sensor_yy,sensor_zz);
-			if( sensor_xx > SENSOR_THRESHOLD[i] || sensor_yy > SENSOR_THRESHOLD[i] || sensor_zz > SENSOR_THRESHOLD[i] )
+			threshold = sensor_calibration[i].threshold;
+			if( sensor_xx > threshold || sensor_yy > threshold || sensor_zz > threshold )
 			{
 				event_report((uint16)(i+1), en_vehicle_comming );
 			}
@@ -84,24 +98,43 @@ int write_sensor_calibration(uint16 DstAddr)
 
 int writefile_sensor_calibration(uint16 DstAddr)
 {
-	FILE *fp;
 	int i = DstAddr - 1;
-	sensor_data_save_t sensor_data_save;
 	
 	pthread_mutex_lock(&sensor_calibration[i].mux);
+
 	sensor_data_save.x = sensor_calibration[i].x;
 	sensor_data_save.y = sensor_calibration[i].y;
 	sensor_data_save.z = sensor_calibration[i].z;	
+	sensor_data_save.threshold = sensor_calibration[i].threshold;
+
 	pthread_mutex_unlock(&sensor_calibration[i].mux);
-	printf(RED"hello,world\r\n"NONE);
-	if((fp = fopen( FILE_DIR, "w" )) == NULL)
+	return 0;	
+}
+
+int write_file_data(void)
+{
+	FILE *fp;
+	int i;
+	if( (fp = fopen( FILE_NAME, "w" )) == NULL )
 	{
-		printf(BLUE"file open faild\r\n"NONE);
+		printf("open file failed\r\n");
 		return -1;
 	}
-	printf(RED"hello,world\r\n"NONE);
-	fwrite( &sensor_data_save, 1, sizeof(sensor_data_save_t), fp);// + i * sizeof(sensor_data_save_t));
-	printf(RED"hello,world\r\n"NONE);
+	fputs("\n", fp );
 	fclose(fp);
-	return 0;	
+
+	for( i = 0 ;i < LOCK_NUM; i++)
+	{ 
+		if(!writefile_sensor_calibration(i + 1))
+		{
+			if(!writeSensorData(i+1, &sensor_data_save))
+				printf(BLUE"write node 0x%04x sensor data to file success\r\n"NONE,i+1);
+			else
+				printf(RED"write node 0x%04x sensor data to file failed\r\n"NONE,i+1);
+		}
+		else
+			printf(RED"lock or unlock mux failed\r\n"NONE);
+	}
+
+	return 0;
 }
--- sensor_data_flow.h
+++ sensor_data_flow.h
@@ -3,9 +3,6 @@
 #include <pthread.h>
 #define LOCK_NUM  5 
 
-#define FILE_DIR "/root/data.dat"
-#define sensor_data_base 0x0000
-
 typedef short int16;
 typedef char int8;
 typedef unsigned short uint16;
@@ -18,21 +15,15 @@ typedef struct _sensor_data_t {
 	int16 x;
 	int16 y;
 	int16 z;
-	uint8 isChecked;
+	int16 threshold;
 	pthread_mutex_t mux;
 } sensor_data_t;
-typedef struct _sensor_data_save_t {
-	int16 x;
-	int16 y;
-	int16 z;
-	int16 threshold;
-} sensor_data_save_t;
 
 void sensor_data_flow_thread(void);
-int judge_car_exist(uint16 DstAddr, int16 x, int16 y, int16 z);
 int write_sensor_update(uint16 DstAddr, int16 x, int16 y, int16 z);
 int write_sensor_calibration(uint16 DstAddr);
 int writefile_sensor_calibration(uint16 DstAddr);
+int write_file_data(void);
 
 #endif
 
